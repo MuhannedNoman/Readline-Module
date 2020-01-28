9165d05d8fd98d389f7ca00ec02096cbc13b806d
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-01-28 06:47:09 +0300

    Handle wrong answer

--- App.js
+++ App.js
@@ -19,25 +19,25 @@
  * HISTORY:
  * Date      	          By	Comments
  * ----------	          ---	----------------------------------------------------------
- * 
+ *
  * .COMPONENT
- *  Required Modules: 
- * 
+ *  Required Modules:
+ *
  * .LICENSE
  * MIT License
- * 
+ *
  * Copyright (c) 2020 Your Company
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
  * the Software without restriction, including without limitation the rights to
  * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
  * of the Software, and to permit persons to whom the Software is furnished to do
  * so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -45,32 +45,43 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- *  
- * 
+ *
+ *
  * #---------------------------------------------------------[Initialisations]--------------------------------------------------------
- * 
+ *
  * #---------------------------------------------------------[Variables]--------------------------------------------------------
- * 
+ *
  * #---------------------------------------------------------[Modules]--------------------------------------------------------
- * 
+ *
  * #---------------------------------------------------------[Functions]--------------------------------------------------------
  */
 
-
-const readline = require('readline');
-const rl = readline.createInterface({input : process.stdin,
-                                    output : process.stdout});
-let random1 = Math.floor((Math.random() * 10) + 1);
-let random2 = Math.floor((Math.random() * 10) + 1);
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+let random1 = Math.floor(Math.random() * 10 + 1);
+let random2 = Math.floor(Math.random() * 10 + 1);
 
 let answer = random1 + random2;
 
-rl.question(`What is ${random1} + ${random2} ? \n`, (userInput) => {
-    if(userInput.trim() == answer){
-        rl.close();
-    }
+rl.question(`What is ${random1} + ${random2} ? \n`, userInput => {
+  if (userInput.trim() == answer) {
+    rl.close();
+  } else {
+    rl.setPrompt("Not correct, try again. \n");
+    rl.prompt();
+    rl.on("line", userInput => {
+      if (userInput.trim() == answer) rl.close();
+      else {
+        rl.setPrompt(`The answer ${userInput} is not correct, try again. \n`);
+        rl.prompt();
+      }
+    });
+  }
 });
 
-rl.on('close',()=>{
-    console.log("Correct answer !!");
+rl.on("close", () => {
+  console.log("Correct answer !!");
 });
